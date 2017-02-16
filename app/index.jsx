09b5481c8450b728231276bd38b1a2cc343c6905
Malik Evans <malikevans393@icloud.com>
2017-02-15 22:53:14 -0500

    WEb

--- app/index.jsx
+++ app/index.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import {render} from 'react-dom';
-import AwesomeComponent from './AwesomeComponent.jsx';
 
 class App extends React.Component {
   render () {
     return (
       <div>
           <h1>Reload Void </h1>
-        <AwesomeComponent />
       </div>
     );
   }

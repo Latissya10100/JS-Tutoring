3956d879fc11ca28906094f3646a8d5446eeeb72
Malik Evans <malikevans393@icloud.com>
2017-02-15 22:30:57 -0500

    New test

--- app/index.jsx
+++ app/index.jsx
@@ -6,7 +6,7 @@ class App extends React.Component {
   render () {
     return (
       <div>
-          <h1>Reload Void app</h1>
+          <h1>Reload Void </h1>
         <AwesomeComponent />
       </div>
     );

ca56b01ee6510a838f24cffb607b6f651cd9c6d8
Malik Evans <malikevans393@icloud.com>
2017-02-27 10:06:43 -0500

    Added Routing

--- app/index.jsx
+++ app/index.jsx
@@ -1,15 +1,45 @@
 import React from 'react';
 import {render} from 'react-dom';
+import { Router, Route, Link, hashHistory, IndexRoute } from 'react-router'
+
+const Index = ({}) =>
+    <div>
+        <h1>Index</h1>
+    </div>;
+
+const Home = ({}) =>
+    <div>
+        <h1>Home</h1>
+    </div>;
+
+const About = ({}) =>
+    <div>
+        <h1>About</h1>
+    </div>;
+
+const NotFound = ({}) =>
+    <div>
+        <h1>NotFound</h1>
+    </div>;
 
 class App extends React.Component {
   render () {
     return (
       <div>
           <h1>Reload Void </h1>
-          <h1>TO</h1>
+          <Link to="/about">About</Link>
+          {this.props.children}
       </div>
     );
   }
 }
 
-render(<App/>, document.getElementById('app'));
+render((
+    <Router history={hashHistory}>
+        <Route path="/" component={App}>
+            <IndexRoute component={Index}/>
+            <Route path="/home" component={Home} />
+            <Route path="/about" component={About} />
+        </Route>
+    </Router>
+), document.getElementById('app'));
